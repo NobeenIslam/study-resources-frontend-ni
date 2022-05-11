86b5ce530f163f9856b258677a8c340e67b3529e
Eralia <eralia.katsakiori@outlook.com>
2022-05-11 16:57:38 +0100

    naive map of resources to resourcesBlock + baseURL to fetch development vs production

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
     <>
       <MainPage />
     </>
-  )
+  );
 }
 
 export default App;
--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,19 +1,17 @@
-import { useState } from "react"
-import Resources from "./Resources"
+import { useState } from "react";
+import Resources from "./Resources";
 
 export default function MainPage(): JSX.Element {
-    const [resourceSearch, setResourceSearch] = useState<string>("")
-    return (
-        <main>
-            <h1>Resources for you</h1>
-            <input
-                placeholder="Search Resources..."
-                value={resourceSearch}
-                onChange={(e) => setResourceSearch(e.target.value)}
-            >
-            </input>
-            <Resources />
-        </main>
-
-    )
+  const [resourceSearch, setResourceSearch] = useState<string>("");
+  return (
+    <main>
+      <h1>Resources for you</h1>
+      <input
+        placeholder="Search Resources..."
+        value={resourceSearch}
+        onChange={(e) => setResourceSearch(e.target.value)}
+      ></input>
+      <Resources />
+    </main>
+  );
 }
