4ff21880191856deb946ec930618c42b8daf6adc
Owen Dearman <owendearman@gmail.com>
2022-05-13 09:30:39 +0100

    added iteration for search helper function

--- src/utils/matchesSearchText.ts
+++ src/utils/matchesSearchText.ts
@@ -4,16 +4,31 @@ export function matchesSearchText(
   resource: ResourceInfo,
   resourceSearch: string
 ): boolean {
-  const isMatchOnTitle = resource.title
-    .toUpperCase()
-    .includes(resourceSearch.toUpperCase());
-  const isMatchOnDesc = resource.description
-    .toUpperCase()
-    .includes(resourceSearch.toUpperCase());
+  const keysToSearch = ["title", "description"];
+  for (const key of keysToSearch) {
+    const resourceSection = resource[key as keyof ResourceInfo];
+    if (typeof resourceSection === "string") {
+      const isMatching = resourceSection
+        .toUpperCase()
+        .includes(resourceSearch.toUpperCase());
+      if (isMatching) {
+        return true;
+      }
+    }
+  }
 
-  /*
+  return false;
+}
+
+// const isMatchOnTitle = resource.title
+//   .toUpperCase()
+//   .includes(resourceSearch.toUpperCase());
+// const isMatchOnDesc = resource.description
+//   .toUpperCase()
+//   .includes(resourceSearch.toUpperCase());
+
+/*
     Need to add functionality to search for tags and for author name
     */
 
-  return isMatchOnTitle || isMatchOnDesc;
-}
+// return isMatchOnTitle || isMatchOnDesc;
