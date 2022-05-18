244d0bb8eba061c5e711b67ea9be1714ef72e62a
Fola <jimiakinwale@icloud.com>
2022-05-18 12:04:49 +0100

    added cypress tests for get requests and clicking resource

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,12 +1,21 @@
 describe('My First Test', () => {
-    it('Visits the mainpage', () => {
+    it('Visits the mainpage loads resources', () => {
+      cy.intercept('https://fone-study-resources.herokuapp.com/resources').as("fetchResource");
       cy.visit('https://fone-study-resources.netlify.app')
-      cy.contains('Resources')
+      cy.wait("@fetchResource");
     })
 
-    it('navigates to resource page', () => {
-        cy.intercept('https://fone-study-resources.herokuapp.com/resource').as("fetchResource")
-        cy.wait("@fetchResource")
-        cy.get("button").eq(2).click()
+    it('checks for user get request', () => {
+        cy.intercept('https://fone-study-resources.herokuapp.com/users').as("fetchUsers");
+      cy.visit('https://fone-study-resources.netlify.app')
+      cy.wait("@fetchUsers");
+      })
+
+    it('Clicks on Go To Youtube Video for resource', () => {
+        cy.intercept('https://fone-study-resources.herokuapp.com/resources').as("fetchResource");
+        cy.visit('https://fone-study-resources.netlify.app')
+        cy.wait("@fetchResource");
+        cy.get("button").contains("Go To Youtube Video").click()
+        //cy.contains("Youtube")
       })
   })
