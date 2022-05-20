45b146904ea076adbfc5e50c2b16859db96830fc
Owen Dearman <owendearman@gmail.com>
2022-05-20 14:57:09 +0100

    added mapping of studylist

--- src/components/StudyList.tsx
+++ src/components/StudyList.tsx
@@ -1,3 +1,35 @@
-export default function StudyList(): JSX.Element {
-  return <p>hello world from StudyList</p>;
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { baseURL } from "../utils/URL";
+import { NoUserInterface, ResourceInfo, UserInterface } from "./Interfaces";
+import SingleResourceBlock from "./SingleResourceBlock";
+
+interface StudyListProps {
+  currentUser: UserInterface | NoUserInterface;
+}
+
+export default function StudyList(props: StudyListProps): JSX.Element {
+  const [studylist, setStudylist] = useState<ResourceInfo[]>([]);
+
+  useEffect(() => {
+    async function fetchStudyList() {
+      console.log(props.currentUser);
+      const studyListRes = await axios.get(
+        `${baseURL}/${props.currentUser.user_id}/studylist`
+      );
+      setStudylist(studyListRes.data);
+    }
+    fetchStudyList();
+  }, [props.currentUser]);
+
+  const mapOfResourcesInStudyList = studylist.map((item) => (
+    <SingleResourceBlock key={item.resource_id} data={item} />
+  ));
+
+  return (
+    <>
+      <h1>Your Study List</h1>
+      <div>{mapOfResourcesInStudyList}</div>
+    </>
+  );
 }
