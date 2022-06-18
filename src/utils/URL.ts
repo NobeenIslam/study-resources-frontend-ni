aa5d7632c479b69d095fac4880eaa67f81ccade6
Nobeen Islam <40375177+NobeenIslam@users.noreply.github.com>
2022-06-18 03:08:14 +0100

    Updated urls to match new apps

--- src/utils/URL.ts
+++ src/utils/URL.ts
@@ -1,9 +1,9 @@
 export const baseURL =
   process.env.NODE_ENV === "production"
-    ? "https://fone-study-resources.herokuapp.com"
+    ? "https://fone-study-resources-ni.herokuapp.com"
     : "http://localhost:4000";
 
 export const frontendURL =
   process.env.NODE_ENV === "production"
-    ? "https://fone-study-resources.netlify.app"
+    ? "https://studyresni.netlify.app/"
     : "http://localhost:3000";
