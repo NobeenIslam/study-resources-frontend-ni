2644e941944eeaba9597c5b33aa29ef6f5bb6436
Nobeen Islam <40375177+NobeenIslam@users.noreply.github.com>
2022-06-19 19:52:11 +0100

    votes date for resource fetched separately and updated on lclick

--- src/components/VotingBar.tsx
+++ src/components/VotingBar.tsx
@@ -8,7 +8,7 @@ interface VotingBarProps {
   currentUser: UserInterface | NoUserInterface;
 }
 
-interface ResourceVoteInfo {
+interface ResourceVoteInfoInterface {
   upVotes: number;
   downVotes: number;
   totalVotes: number;
@@ -16,37 +16,59 @@ interface ResourceVoteInfo {
 
 export function VotingBar(props: VotingBarProps): JSX.Element {
   const initialVoteInfo = { upVotes: 0, downVotes: 0, totalVotes: 0 };
-  const [votes, setVotes] = useState<ResourceVoteInfo>(initialVoteInfo);
-  const [fetchVotesToggle, setVotesToggle] = useState<boolean>(false);
+  const [resourceVoteInfo, setResourceVoteInfo] =
+    useState<ResourceVoteInfoInterface>(initialVoteInfo);
+  const [fetchVoteInfoToggle, setFetchVoteInfoToggle] =
+    useState<boolean>(false);
+  const [whichButtonActiveClass, setWhichButtonActiveClass] = useState<
+    string[]
+  >(["", ""]);
+
+  console.log(whichButtonActiveClass, props.data.title);
 
   useEffect(() => {
     async function fetchResourceVotes() {
       const response = await axios.get(
         `${baseURL}/resources/${props.data.resource_id}/votes`
       );
-      console.log(response.data);
-      setVotes(response.data);
+      setResourceVoteInfo(response.data);
     }
     fetchResourceVotes();
-  }, [fetchVotesToggle]);
+  }, [fetchVoteInfoToggle, props.data.resource_id]);
 
-  async function handleClickVote(isUpvote: boolean) {
+  async function handleClickVote(isUpvote: boolean, buttonClasses: string[]) {
     if (props.currentUser.user_id === "not-signed-in") {
       return;
     }
-    axios.post(`${baseURL}/resources/${props.data.resource_id}/votes`, {
-      user_id: props.currentUser.user_id,
-      is_upvote: isUpvote,
-    });
+    const response = await axios.post(
+      `${baseURL}/resources/${props.data.resource_id}/votes`,
+      {
+        user_id: props.currentUser.user_id,
+        is_upvote: isUpvote,
+      }
+    );
+    setFetchVoteInfoToggle(!fetchVoteInfoToggle);
+    setWhichButtonActiveClass(buttonClasses);
   }
 
   return (
     <section className="d-flex flex-column align-items-center justify-content-evenly ms-3">
-      <p>({props.data.votesInfo.upVotes})</p>
-      <button onClick={() => handleClickVote(true)}>👍</button>
-      <p>({props.data.votesInfo.totalVotes})</p>
-      <button onClick={() => handleClickVote(true)}>👎</button>
-      <p>({props.data.votesInfo.downVotes})</p>
+      <p className="text-success">({resourceVoteInfo.upVotes})</p>
+      <button
+        data-toggle="button"
+        className={`btn btn-success ${whichButtonActiveClass[0]} active`}
+        onClick={() => handleClickVote(true, ["active", ""])}
+      >
+        👍
+      </button>
+      <p>({resourceVoteInfo.totalVotes})</p>
+      <button
+        className={`btn btn-danger ${whichButtonActiveClass[1]}`}
+        onClick={() => handleClickVote(false, ["", "active"])}
+      >
+        👎
+      </button>
+      <p className="text-danger">({resourceVoteInfo.downVotes})</p>
     </section>
   );
 }
